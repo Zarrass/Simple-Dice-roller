93f32c1f494992c3bf23121a6aea3bbd041f3128
Zarrass <35363369+Zarrass@users.noreply.github.com>
2022-01-18 13:23:38 +0700

    Add D20

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -13,12 +13,22 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
 
+        val rollButton20: Button = findViewById(R.id.button2)
+        rollButton20.setOnClickListener { rollDice20() }
+
     }
 
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
         val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+
+    private fun rollDice20() {
+        val dice2 = Dice(20)
+        val diceRoll = dice2.roll()
+        val resultTextView: TextView = findViewById(R.id.textView2)
         resultTextView.text = diceRoll.toString()
     }
 }
